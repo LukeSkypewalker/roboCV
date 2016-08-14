f68844137dc37411e30be7c9547e9de406688c55
LukeSkypewalker <work37@gmail.com>
2016-08-14 19:23:40 +0300

    fixed
    problem was in extra nodes and in shallow copy of map

--- z_star.py
+++ z_star.py
@@ -1,6 +1,8 @@
 from collections import namedtuple
+from copy import deepcopy
 from pprint import pprint
 from roboCV.maps import grid
+from roboCV.robot import Robot
 
 
 class Node:
@@ -17,23 +19,16 @@ class Node:
         return '(%d %d %d %d %d %d)' % (self.y, self.x, self.z, self.g, self.h, self.score)
 
 
-class Robot:
-    def __init__(self, path):
-        self.path = path
+def calc_heuristic(a, b):
+    return abs(b.y - a.y) + abs(b.x - a.x)
+    # return sqrt((b.x - a.x)**2 + (b.y - a.y)**2)
 
 
-def calc_heuristic(node, end):
-    return abs(end.y - node.y) + abs(end.x - node.x)
-    # return sqrt((end.x - node.x)**2 + (end.y - node.y)**2)
-
-
-def get_neighbors_nodes(_map, current_pos, closedset):
-    return [Node(*target_pos) for target_pos in surround_area(current_pos)
-            if is_free(target_pos, _map, current_pos, closedset)]
-
-
-def is_free(cell, _map, current_pos, visited_set):
-    return is_not_wall(cell, _map) and is_not_visited(cell, visited_set) and is_not_crossed(cell, current_pos)
+def get_neighbor_cells(_map, current_pos, closed_list):
+    return [target_pos for target_pos in surround_area(current_pos)
+            if (is_not_wall(target_pos, _map) and
+                not is_in_list(target_pos, closed_list) and
+                is_not_crossed(target_pos, current_pos))]
 
 
 def is_not_wall(_position, _map):
@@ -41,17 +36,20 @@ def is_not_wall(_position, _map):
     width = len(_map[0])
     y = _position[0]
     x = _position[1]
-    return 0 <= x < width and 0 <= y < height and not _map[y][x]
+    return 0 <= y < height and 0 <= x < width and not _map[y][x]
 
 
-def is_not_visited(cell, closedset):
-    for i in closedset:
-        if i.y == cell[0] and i.x == cell[1]:
-            return False
-    return True
+def is_in_list(pos, list):
+    for item in list:
+        if is_same_pos(item, pos):
+            return True
+    return False
 
 
-robots = []
+def is_same_pos(a, b):
+    if a.y == b[0] and a.x == b[1] and a.z == b[2]:
+        return True
+    return False
 
 
 def is_not_crossed(target_pos, current_pos):
@@ -60,12 +58,6 @@ def is_not_crossed(target_pos, current_pos):
             if is_same_pos(point, target_pos) or is_x_crosed(target_pos, current_pos, point):
                 return False
     return True
-
-
-def is_same_pos(a, b):
-    if a.y == b[0] and a.x == b[1] and a.z == b[2]:
-        return True
-    return False
 
 
 def is_x_crosed(target_pos, current_pos, point):
@@ -80,8 +72,7 @@ Coords = namedtuple('Coords', 'y x z')
 # So, there are 5 cells, including stay on the same place
 def surround_area(current_pos):
     y, x, z = current_pos.y, current_pos.x, current_pos.z
-    return [(y - 1, x, z + 1), (y, x + 1, z + 1), (y + 1, x, z + 1), (y, x - 1, z + 1),
-            (y, x, z + 1)]  # , (y, x, z + 1)]stay and wait
+    return [(y - 1, x, z + 1), (y, x + 1, z + 1), (y + 1, x, z + 1), (y, x - 1, z + 1), (y, x, z + 1)]
 
 
 def reconstruct_path_(current):
@@ -89,66 +80,71 @@ def reconstruct_path_(current):
     while current.parent:
         path.append(current)
         current = current.parent
-    # path.append(current)
     return path[::-1]
 
 
+def find_node(cell, list):
+    for item in list:
+        if is_same_pos(item, cell):
+            return item
+    return None
+
+
 def search(grid, start, end):
-    openset = set()
-    closedset = set()
-    openset.add(start)
+    openlist = []
+    closedlist = []
+    openlist.append(start)
 
-    while openset:
-        current_pos = min(openset, key=lambda o: (o.score, o.h))
+    while openlist:
+        current_pos = min(openlist, key=lambda o: (o.score, o.h))
 
-        if current_pos.x == end.x and current_pos.y == end.y:
+        if current_pos.y == end.y and current_pos.x == end.x:
             return reconstruct_path_(current_pos)
 
-        openset.remove(current_pos)
-        closedset.add(current_pos)
+        openlist.remove(current_pos)
+        closedlist.append(current_pos)
 
-        for node in get_neighbors_nodes(grid, current_pos, closedset):
+        for cell in get_neighbor_cells(grid, current_pos, closedlist):
             new_g = current_pos.g + 1  # current.move_cost(node)
-            if node in openset:
+            if is_in_list(cell, openlist):
+                node = find_node(cell, openlist)
                 if node.g > new_g:
                     node.g = new_g
                     node.parent = current_pos
             else:
+                node = Node(*cell)
                 node.g = new_g
                 node.h = calc_heuristic(node, end)
                 node.score = node.g + node.h
                 node.parent = current_pos
-                openset.add(node)
+                openlist.append(node)
+                print(node)
     return []
 
 
+robots = []
+
+
 if __name__ == '__main__':
-    start = Node(1, 3, 0)
-    end = Node(6, 3, 0)
-    path = search(grid, start, end)
-    print('path1')
-    pprint(path)
+    robots.append(Robot('Wall-e', (1, 3), (6, 3)))
+    robots.append(Robot('R2D2', (6, 3), (1, 3)))
+    map_path = deepcopy(grid)
 
-    robots.append(Robot(path))
+    for robot in robots:
 
-    start = Node(6, 3, 0)
-    end = Node(1, 3, 0)
-    path2 = search(grid, start, end)
-    print('path2')
-    pprint(path2)
+        start = Node(*robot.src, 0)
+        end = Node(*robot.dst, 0)
+        robot.path = search(grid, start, end)
+        print(robot.name)
+        pprint(robot.path)
 
-    map_path = grid.copy()
-    for coords in path:
-        map_path[coords.y][coords.x] = 2
-    for coords in path2:
-        map_path[coords.y][coords.x] += 3
+        for point in robot.path:
+            map_path[point.y][point.x] += 1
 
     import matplotlib.pyplot as plt
-
     plt.figure(1)
     plt.imshow(map_path, interpolation='nearest', cmap='Blues')
     plt.grid(True)
     plt.xticks(range(len(grid[0])))
     plt.yticks(range(len(grid)))
-
     plt.show()
