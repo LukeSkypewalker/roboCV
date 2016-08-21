c214cf90e4e94a6eb09108d7e7e383732e2df730
LukeSkypewalker <work37@gmail.com>
2016-08-21 14:24:28 +0300

    strange buy b12(right bottom are blocked)

---
+++ visualization.py
@@ -0,0 +1,30 @@
+from copy import deepcopy
+
+
+def visualize(grid, robots):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.animation as animation
+
+    fig2 = plt.figure()
+    plt.grid(True)
+    plt.xticks(range(len(grid[0])))
+    plt.yticks(range(len(grid)))
+
+    frames = []
+    n = max([len(robot.path) for robot in robots])
+    for j in np.arange(n):
+        frame = deepcopy(grid)
+        for i, robot in enumerate(robots):
+            if j < len(robot.path):
+                frame[robot.path[j].y][robot.path[j].x] = i + 2
+            else:
+                frame[robot.path[-1].y][robot.path[-1].x] = i + 2
+
+        frames.append((plt.imshow(frame, interpolation='nearest', cmap='jet'),))
+
+    ani = animation.ArtistAnimation(fig2, frames, interval=500, repeat_delay=0, blit=False)
+    plt.rcParams['animation.ffmpeg_path'] = 'D:\\SOFT\\ffmpeg\\bin\\ffmpeg'
+    FFwriter = animation.FFMpegWriter()
+    ani.save('im.mp4', writer=FFwriter, fps=30)
+    plt.show()
--- z_star.py
+++ z_star.py
@@ -1,8 +1,9 @@
 from collections import namedtuple
-from copy import deepcopy
 from pprint import pprint
+
 from roboCV.maps import grid
 from roboCV.robot import Robot
+from roboCV.visualization import visualize
 
 
 class Node:
@@ -10,25 +11,34 @@ class Node:
         self.y = y
         self.x = x
         self.z = z
-        self.g = 0
-        self.h = 0
+        self.g_cost = 0
+        self.h_cost = 0
         self.score = 0
         self.parent = None
 
     def __repr__(self):
         return '(%d %d %d)' % (self.y, self.x, self.z)
-        # return '(%d %d %d %d %d %d)' % (self.y, self.x, self.z, self.g, self.h, self.score)
+        # return '(%d %d %d %d %d %d)' % (self.y, self.x, self.z, self.g_cost, self.h_cost, self.score)
 
 
-def calc_heuristic(a, b):
-    return abs(b.y - a.y) + abs(b.x - a.x)
-    # return sqrt((b.x - a.x)**2 + (b.y - a.y)**2)
+Coords = namedtuple('Coords', 'y x z')
 
 
-def get_neighbor_cells(_map, current_pos, closed_list, robot):
+# TBC: 8 neighbors is tricky because diagonal movement is x1.4 times longer, but we should stay synchronized
+# So, there are 5 cells, including stay on the same place
+def surround_area(current_pos):
+    y, x, z = current_pos.y, current_pos.x, current_pos.z
+    return [Coords(y - 1, x, z + 1),
+            Coords(y, x + 1, z + 1),
+            Coords(y + 1, x, z + 1),
+            Coords(y, x - 1, z + 1),
+            Coords(y, x, z + 1)]  # stay on the same place
+
+
+def neighbor_cells(_map, current_pos, closed_list, robot):
     return [target_pos for target_pos in surround_area(current_pos)
             if (is_not_wall(target_pos, _map) and
-                not is_in_list(target_pos, closed_list) and
+                not is_in_list(closed_list, target_pos) and
                 is_not_crossed(current_pos, target_pos, robot))]
 
 
@@ -40,7 +50,7 @@ def is_not_wall(_position, _map):
     return 0 <= y < height and 0 <= x < width and not _map[y][x]
 
 
-def is_in_list(pos, list):
+def is_in_list(list, pos):
     for item in list:
         if is_same_pos(item, pos):
             return True
@@ -62,10 +72,8 @@ def is_not_crossed(current_pos, target_pos, this_robot):
             continue
 
         point = robot.path[z] if z < len(robot.path) else robot.path[-1]
-        # print(z, point, current_pos, target_pos)
 
         if is_same_pos(Coords(point.y, point.x, z), target_pos):
-            # print('alarm', point, target_pos)
             return False
 
         if z < len(robot.path):
@@ -74,37 +82,17 @@ def is_not_crossed(current_pos, target_pos, this_robot):
     return True
 
 
-# def is_not_crossed2(target_pos, current_pos):
-#     z = target_pos.z
-#
-#     for robot in robots:
-#         for point in robot.path:
-#             if is_same_pos(point, target_pos) or is_x_crosed(target_pos, current_pos, point):
-#                 return False
-#
-#     return True
-
-
 def is_x_crosed(target_pos, current_pos, point):
     return (is_same_pos(point.parent, Coords(target_pos.y, target_pos.x, target_pos.z - 1)) and
             is_same_pos(point, Coords(current_pos.y, current_pos.x, current_pos.z + 1)))
 
 
-Coords = namedtuple('Coords', 'y x z')
+def calc_heuristic(a, b):
+    return abs(b.y - a.y) + abs(b.x - a.x)
+    # return sqrt((b.x - a.x)**2 + (b.y - a.y)**2)
 
 
-# TBC: 8 neighbors is tricky because diagonal movement is x1.4 times longer, but we should stay synchronized
-# So, there are 5 cells, including stay on the same place
-def surround_area(current_pos):
-    y, x, z = current_pos.y, current_pos.x, current_pos.z
-    return [Coords(y - 1, x, z + 1),
-            Coords(y, x + 1, z + 1),
-            Coords(y + 1, x, z + 1),
-            Coords(y, x - 1, z + 1),
-            Coords(y, x, z + 1)]
-
-
-def reconstruct_path_(current_pos):
+def reconstruct_path(current_pos):
     path = []
     while current_pos.parent:
         path.append(current_pos)
@@ -119,76 +107,56 @@ def find_node(cell, list):
     return None
 
 
+def calc_heuristic(a, b):
+    return abs(b.y - a.y) + abs(b.x - a.x)
+    # return sqrt((b.x - a.x)**2 + (b.y - a.y)**2)
+
+
 def search(grid, robot):
-    start = robot.path[0]
-    end = robot.dst
     openlist = []
     closedlist = []
-    openlist.append(start)
+    openlist.append(robot.path[0])
+
+    # duct tape
+    max_path_len_list = [len(robo.path) for robo in robots]
+    max_path_len_list.append(0)  # otherwise empty list error
+    z_max = max(max_path_len_list)-1  # index offset
 
     while openlist:
-        current_pos = min(openlist, key=lambda o: (o.score, o.h))
-
-        if current_pos.y == end.y and current_pos.x == end.x:
-            return reconstruct_path_(current_pos)
-
+        current_pos = min(openlist, key=lambda o: (o.score, o.h_cost))
         openlist.remove(current_pos)
         closedlist.append(current_pos)
 
-        for cell in get_neighbor_cells(grid, current_pos, closedlist, robot):
-            if cell.y == current_pos.y and cell.x == current_pos.x:
-                move_cost = current_pos.g + 0.5
-            else:
-                move_cost = current_pos.g + 1  # current.move_cost(node)
+        for target_pos in neighbor_cells(grid, current_pos, closedlist, robot):
 
-            if is_in_list(cell, openlist):
-                node = find_node(cell, openlist)
-                if node.g > move_cost:
-                    node.g = move_cost
-                    node.parent = current_pos
+            if target_pos.z >= z_max and target_pos.y == robot.dst.y and target_pos.x == robot.dst.x:
+                end = Node(*target_pos)
+                end.parent = current_pos
+                return reconstruct_path(end)
+
+            new_g_cost = current_pos.g_cost + (0.5
+                if target_pos.y == current_pos.y and target_pos.x == current_pos.x
+                else 1)
+
+            if is_in_list(openlist, target_pos):
+                node = find_node(target_pos, openlist)
+                if new_g_cost >= node.g_cost:
+                    continue
             else:
-                node = Node(*cell)
-                node.g = move_cost
-                node.h = calc_heuristic(node, end)
-                node.score = node.g + node.h
-                node.parent = current_pos
+                node = Node(*target_pos)
                 openlist.append(node)
+                node.h_cost = calc_heuristic(node, robot.dst)
+
+            node.g_cost = new_g_cost
+            node.score = new_g_cost + node.h_cost
+            node.parent = current_pos
     return []
-
-
-def visualize(grid, robots):
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import matplotlib.animation as animation
-
-    fig2 = plt.figure()
-    plt.grid(True)
-    plt.xticks(range(len(grid[0])))
-    plt.yticks(range(len(grid)))
-
-    frames = []
-    n = max([len(robot.path) for robot in robots])
-    for j in np.arange(n):
-        frame = deepcopy(grid)
-        for i, robot in enumerate(robots):
-            if j < len(robot.path):
-                frame[robot.path[j].y][robot.path[j].x] = i + 2
-            else:
-                frame[robot.path[-1].y][robot.path[-1].x] = i + 2
-
-        frames.append((plt.imshow(frame, interpolation='nearest', cmap='jet'),))
-
-    ani = animation.ArtistAnimation(fig2, frames, interval=500, repeat_delay=0, blit=False)
-    plt.rcParams['animation.ffmpeg_path'] = 'D:\\SOFT\\ffmpeg\\bin\\ffmpeg'
-    FFwriter = animation.FFMpegWriter()
-    ani.save('im.mp4', writer=FFwriter, fps=30)
-    plt.show()
 
 
 if __name__ == '__main__':
     m = len(grid) - 1
     n = len(grid[0]) - 1
-    # TODO: Check for border and obstacles
+    # TODO: Check for borders and obstacles before creating the robot
     robots = []
     bots = []
     # bots.append(Robot('Wall-e', Node(0, 0), Node(m, n)))
@@ -196,11 +164,11 @@ if __name__ == '__main__':
     # robots.append(Robot('Zumo', Node(m, 0), Node(0, n)))
     # robots.append(Robot('R2D2', Node(0, n), Node(m, 0)))
 
-    for i in range(10 + 1):
+    for i in range(12 + 1):
         bots.append(Robot('a' + str(i), Node(m - i, 0), Node(i, n)))
         bots.append(Robot('b' + str(i), Node(i, n), Node(m - i, 0)))
-        # bots.append(Robot('a' + str(i), Node(m - i, 1), Node(i, n-1)))
-        # bots.append(Robot('b' + str(i), Node(i, n-1), Node(m - i, 1)))
+        bots.append(Robot('c' + str(i), Node(m - i, 1), Node(i, n-1)))
+        bots.append(Robot('d' + str(i), Node(i, n-1), Node(m - i, 1)))
 
     for robot in bots:
         print(robot.name)
@@ -208,4 +176,5 @@ if __name__ == '__main__':
         [robot.path.append(point) for point in path]
         pprint(robot.path)
         robots.append(robot)
+
     visualize(grid, robots)
